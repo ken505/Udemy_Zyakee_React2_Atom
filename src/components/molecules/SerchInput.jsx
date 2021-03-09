d0812ca1ac3b1d324788f701263af772ac4b177c
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-09 21:11:39 +0900

    28_done:

--- src/components/molecules/SerchInput.jsx
+++ src/components/molecules/SerchInput.jsx
@@ -1,10 +1,24 @@
+import styled from "styled-components";
+
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { Input } from "../atoms/input/Input";
 
 export const SerchInput = () => {
   return (
-    <div>
-      <input placeholder="検索条件を入力" />
-      <PrimaryButton>Serch</PrimaryButton>
-    </div>
+    <SContainer>
+      <Input placeholder="検索条件を入力" />
+      <SButtonWrapper>
+        <PrimaryButton>Serch</PrimaryButton>
+      </SButtonWrapper>
+    </SContainer>
   );
 };
+
+const SContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const SButtonWrapper = styled.div`
+  padding-left: 8px;
+`;
