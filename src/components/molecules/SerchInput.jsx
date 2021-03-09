ea1c6e4b0350d7b3caf7b3503eb20f370cc495d7
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-09 19:00:11 +0900

    28_wip:

---
+++ src/components/molecules/SerchInput.jsx
@@ -0,0 +1,10 @@
+import { PrimaryButton } from "../atoms/button/PrimaryButton";
+
+export const SerchInput = () => {
+  return (
+    <div>
+      <input placeholder="検索条件を入力" />
+      <PrimaryButton>Serch</PrimaryButton>
+    </div>
+  );
+};
