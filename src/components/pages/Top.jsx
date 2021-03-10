704ce9b8e5214d826cbe49ed35231a1e4d86f762
ken505 <68226398+ken505@users.noreply.github.com>
2021-03-10 15:20:26 +0900

    wip:

--- src/components/pages/Top.jsx
+++ src/components/pages/Top.jsx
@@ -1,9 +1,17 @@
+import React from "react";
 import styled from "styled-components";
+import { SecondaryButton } from "../atoms/button/SecondaryButton";
 
 export const Top = () => {
+  const onClickAdmin = () => alert("root");
+  const onClickGeneral = () => alert("general");
   return (
     <SContainer>
       <h2>Top page</h2>
+      <SecondaryButton onClick={onClickAdmin}>Root user</SecondaryButton>
+      <br />
+      <br />
+      <SecondaryButton onClick={onClickGeneral}>General user</SecondaryButton>
     </SContainer>
   );
 };
